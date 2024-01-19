83ba55a5e765d063c9c9c2d8f485f8b722abda29
franklinTaylor <ftaylor07@icloud.com>
2024-01-19 11:59:20 -0500

    added and finished my problems on the index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-// get total count of fruits from and object using .reduce
+// get sum of all numbers in an input array using .reduce
 
+function sumOfArr(arr) {
+    return arr.reduce((acc, currVal, ) => acc + currVal, 0 );
+}
+
+
+
+let arr1 = [54,6,78,5,67,8,56,45,2]
+console.log(sumOfArr(arr1));
+
+
+// # 2 create a fruit object with fig: 9 , raspberr: 67, apple: 6, nectarine: 20, kiwi:5 as your properties and  get total count of fruits from and object using .reduce
 
 const fruits = {
     fig: 9,
@@ -9,17 +20,17 @@ const fruits = {
     kiwi: 5,
 }
 
-const totalFruit = Object.entries(fruits).reduce((acc, [fruits, num]) => {
-    return acc + num },); 
+const totalFruit = Object.entries(fruits).reduce((acc, [fruit, num]) => {
+    return acc + num 
+}, 0); 
+
+
+
     
     //  Object.entries  convert obj ---> to  (nested arrays  of [key , value] ) pair
     // .reduce() itterates through the nested array pairs [fruits, num]
     // -------------------------------------------ex:      [apple, 6]
     //-------------------------------------------->        [nectarine, 20]
-    // return acc + count is how i get the count  buyy adding 
-    
-
-    
-console.log(totalFruit);
-
-
+    // return acc + count is how i get the count  buyy adding my acculator ( acc )
+      
+console.log(totalFruit);// 107
